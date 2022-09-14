8e5093e80cb3778a7db35eb99458ecf3b088f49c
komangputrasn <112533241+komangputrasn@users.noreply.github.com>
2022-09-14 20:27:04 +0700

    Add files via upload

---
+++ pertemuan 3/list2.py
@@ -0,0 +1,5 @@
+warna = ['merah', 'kuning', 'hijau', 'biru', 'hitam', 'putih']
+
+print(warna[0])
+print(warna[1])
+print(warna[2])
---
+++ pertemuan 3/list3.py
@@ -0,0 +1,3 @@
+mahasiswa = ['komang', '2021071015', 'informatika', 'desain dan analisis algoritma', '14-9-2022', 'universitas pembangunan jaya']
+print(mahasiswa[1])
+
